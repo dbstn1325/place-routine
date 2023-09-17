ef45bba85173aa8cf2b88d55f63e07566f883e49
dbstn1325 <dbstn1325@naver.com>
2023-09-17 23:16:46 +0900

    feat: Category 도메인 검증 [feat]: [Category 도메인 검증] - category 객체 생성 시, 공백인 경우 - category 객체 생성 시, 이름의 길이가 20을 초과할 경우 - 위 2가지 경우에 대해 검증 로직 추가 및 테스트 케이스 작성 완료
    -

--- backend/src/main/java/com/pr/category/domain/Category.java
+++ backend/src/main/java/com/pr/category/domain/Category.java
@@ -1,7 +1,9 @@
 package com.pr.category.domain;
 
+import com.pr.category.exception.InvalidCategoryException;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -10,7 +12,9 @@ import javax.persistence.*;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Getter
 public class Category {
+    public static final int MAX_NAME_LENGTH = 20;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +25,16 @@ public class Category {
     private String name;
 
     public Category(String name) {
+        validateNameLength(name);
         this.name = name;
+    }
+
+    private void validateNameLength(final String name) {
+        if (name.isBlank()) {
+            throw new InvalidCategoryException("카테고리 이름은 공백일 수 없습니다.");
+        }
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new InvalidCategoryException(String.format("카테고리 이름의 길이는 %d을 초과할 수 없습니다.", MAX_NAME_LENGTH));
+        }
     }
 }
--- backend/src/test/java/com/pr/category/CategoryTest.java
+++ backend/src/test/java/com/pr/category/CategoryTest.java
@@ -1,8 +1,11 @@
 package com.pr.category;
 
 import com.pr.category.domain.Category;
+import com.pr.category.exception.InvalidCategoryException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,4 +32,15 @@ class CategoryTest {
         assertThatThrownBy(() -> new Category(name))
                 .isInstanceOf(InvalidCategoryException.class);
     }
+
+    @DisplayName("카테고리 이름의 길이가 20을 초과하는 경우 예외를 던진다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"일이삼사오육칠팔구십일이삼사오육칠팔구십일",
+            "플레이스 루틴 플레이스 루틴 플레이스 루틴 카테고리"})
+    void 카테고리_이름의_길이가_20을_초과하는_경우_예외를_던진다(final String name) {
+        // given & when & then
+        assertThatThrownBy(() -> new Category(name))
+                .isInstanceOf(InvalidCategoryException.class);
+    }
+
 }
