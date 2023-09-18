9fcb7d1711038ec6734dd759271ff61d9fa3ecbc
dbstn1325 <dbstn1325@naver.com>
2023-09-18 09:51:35 +0900

    test(CategoryService): 카테고리 생성 시, 공백이거나 길이 20 일 때 예외 검증

--- backend/src/test/java/com/pr/category/domain/CategoryServiceTest.java
+++ backend/src/test/java/com/pr/category/domain/CategoryServiceTest.java
@@ -2,10 +2,13 @@ package com.pr.category.domain;
 
 import com.pr.category.dto.request.CategoryCreateRequest;
 import com.pr.category.dto.response.CategoryResponse;
+import com.pr.category.exception.InvalidCategoryException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -35,5 +38,15 @@ class CategoryServiceTest {
 
         //then
         assertThat(actual.getName()).isEqualTo(조깅_카테고리_이름);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "일이삼사오육칠팔구십일이삼사오육칠팔구십일", "플레이스 루틴 플레이스 루틴 플레이스 루틴 카테고리"})
+    void 카테고리를_생성할_때_이름이_공백이거나_길이가_20을_초과하면_예외가_발생한다(final String invalidName) {
+        CategoryCreateRequest 카테고리_생성_요청 = new CategoryCreateRequest(invalidName);
+
+        // when & then
+        assertThatThrownBy(() -> categoryService.save(카테고리_생성_요청))
+                .isInstanceOf(InvalidCategoryException.class);
     }
 }
