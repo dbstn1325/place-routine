3ad63c9c1de39a2fa747fc245bf6a142397b2002
dbstn1325 <dbstn1325@naver.com>
2023-09-19 17:11:51 +0900

    test: CategoryTest 진행 시, 중복 검증 버그 수정 [test]: [CategoryTest 진행 시, 중복 검증 버그 수정] - CategoryServiceTest 전체 실행 시, 중복되는 카테고리 생성 검증에서의 .save() 때 예외가 터져버림 - databaseCleaner를 통해 테이블 공간 자체를 제거
    -

--- backend/src/test/java/com/pr/category/application/CategoryServiceTest.java
+++ backend/src/test/java/com/pr/category/application/CategoryServiceTest.java
@@ -1,10 +1,12 @@
-package com.pr.category.domain;
+package com.pr.category.application;
 
-import com.pr.category.application.CategoryService;
+import com.pr.category.domain.CategoryRepository;
 import com.pr.category.dto.request.CategoryCreateRequest;
 import com.pr.category.dto.response.CategoryResponse;
 import com.pr.category.exception.ExistCategoryException;
 import com.pr.category.exception.InvalidCategoryException;
+import com.pr.common.DatabaseCleaner;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,6 +14,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Table;
+import javax.persistence.metamodel.Type;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.pr.common.Constants.조깅_카테고리_이름;
 import static org.assertj.core.api.Assertions.*;
@@ -27,7 +36,15 @@ class CategoryServiceTest {
     private CategoryService categoryService;
 
     @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @Autowired
     private CategoryRepository categoryRepository;
+
+    @BeforeEach
+    void setUp() {
+        databaseCleaner.execute();
+    }
 
     @DisplayName("카테고리를 생성한다.")
     @Test
