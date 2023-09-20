e9aef7cc87ab5860ebfc740780d4793866f9ca60
dbstn1325 <dbstn1325@naver.com>
2023-09-20 11:42:38 +0900

    refactor: GlobalStyle 위치 App 최상단 변경

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import GlobalStyle from "./globalStyle";
 import { HomePage } from "./pages/Home";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+const LoginPage = () =>
+  import(
+    /* webpackChunkName: "Login" */ /* webpackPrefetch: true */ "./pages/Login"
+  );
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle />
       <HomePage />
     </div>
   );
