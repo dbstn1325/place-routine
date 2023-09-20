5259f55f8f4b4ae45147eb567433a0e488b68c9f
dbstn1325 <dbstn1325@naver.com>
2023-09-20 16:32:53 +0900

    feat: HomePage 라우팅 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import { HomePage } from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ROUTE } from "./constants/route";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route exact path={ROUTE.HOME.PATH} element={<HomePage />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
